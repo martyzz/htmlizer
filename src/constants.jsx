b423a46c87b32ad1a63b705390f3a0dc2beae556
Martin Mach <machmarti@gmail.com>
2018-01-30 13:32:00 +0100

    initial commit 2

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-export const API_ROOT = "https://machmartin.cz/apps/htmlizer-backend/api";
+export const API_ROOT = "<API_ROOT_URL>";
 export const CUSTOM_NODE_TYPE = "htmlizer/CUSTOM_NODE_TYPE";
 export const CODE_MIRROR_OPTIONS = {
   lineNumbers: true,
